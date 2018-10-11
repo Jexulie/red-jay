077e244737262b0a2cc9715a3a3398ada627b818
Jexulie <fejitj3n@yahoo.com>
2018-10-11 12:31:52 +0300

    almost

---
+++ redjay.js
@@ -0,0 +1,42 @@
+const fileHandler = require('./lib/fileHandler');
+const consoleOutput = require('./lib/consoleOutput');
+
+function redJay(){}
+
+redJay.prototype.prod = function(req, res, next){
+    var start = new Date().getTime();
+    var end;
+    res.on('finish', () => { 
+        end = new Date().getTime();
+        const REQUEST = {
+            response: (end - start),
+            content: req.headers['content-length'],
+            headers: req.headers['user-agent'],
+            url: req.url,
+            method: req.method
+        }
+        fileHandler.writeToFile('info', REQUEST);     
+    });   
+    next(); 
+}
+
+redJay.prototype.dev = function(req, res, next){
+    var start = new Date().getTime();
+    var end;
+    res.on('finish', () => { 
+        end = new Date().getTime();
+        console.log(end, start , end - start)
+        const REQUEST = {
+            response: (end - start),
+            content: req.headers['content-length'],
+            headers: req.headers['user-agent'],
+            url: req.url,
+            method: req.method
+        }
+        fileHandler.writeToFile('info', REQUEST);
+        consoleOutput(REQUEST);      
+    });
+    next();
+}
+
+module.exports = new redJay();
